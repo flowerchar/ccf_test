350220141333cd39f9e3ac7a21626e1e7db781b2
unknown <flowerchar2001@gmail.com>
2024-07-05 21:27:14 +0800

    feat: 忽略统计html

--- interface_auto/interface_frame/apis/ceshiren_search.py
+++ interface_auto/interface_frame/apis/ceshiren_search.py
@@ -1,6 +1,5 @@
 """
-__author__ = '霍格沃兹测试开发学社'
-__desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/topic/15860'
+ 
 """
 from interface_auto.interface_frame.apis.ceshiren import Ceshiren
 
--- web_auto/web_po/page/main_page.py
+++ web_auto/web_po/page/main_page.py
@@ -1,6 +1,5 @@
 """
-__author__ = '霍格沃兹测试开发学社'
-__desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/topic/15860'
+ 
 """
 from selenium.webdriver.common.by import By
 from web_auto.web_po.base.base_page import BasePage
